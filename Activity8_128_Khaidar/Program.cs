6f31ce1856718abfb7a4f5cd4b5f3b4205110eb8
TA1128 <m.khaidar.ft21@mail.umy.ac.id>
2022-12-28 17:59:59 +0700

    add static void main

--- Activity8_128_Khaidar/Program.cs
+++ Activity8_128_Khaidar/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Security;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace BinarySearchTree_128
@@ -6,9 +7,152 @@ namespace BinarySearchTree_128
     class Node
     {
         public string info;
-        public string leftchild;
-        public string rightchild;
+        public Node leftchild;
+        public Node rightchild;
 
+
+        public Node(string i, Node l, Node r)
+        {
+            info = i;
+            leftchild = l;
+            rightchild = r;
+        }
+    }
+
+    class program
+    {
+        public Node ROOT;
+        public program()
+        {
+            ROOT = null;
+        }
+        public void search(string element, ref Node parent, ref Node currentNode)
+        {
+            currentNode = ROOT;
+            parent = null;
+            while((currentNode != null) && (currentNode.info !=element))
+            {
+                parent = currentNode;
+                if (string.Compare(element, currentNode.info) < 0)
+                    currentNode = currentNode.leftchild;
+                else
+                    currentNode = currentNode.rightchild;
+            }
+        }
+        public void insert(string element)
+        {
+            Node tmp, parent = null, currentNode = null;
+            search(element, ref parent, ref currentNode);
+            if (currentNode != null)
+            {
+                Console.WriteLine("Duplicate words not allowed");
+                return;
+            }
+            else
+            {
+                tmp = new Node(element, null, null);
+                if (parent == null)
+                {
+                    ROOT = tmp;
+                }
+                else if (string.Compare(element, parent.info) < 0)
+                {
+                    parent.leftchild = tmp;
+                }
+                else
+                {
+                    parent.rightchild = tmp;
+                }
+            }
+        }
+        public void inorder(Node ptr)
+        {
+            if (ROOT == null)
+            {
+                Console.WriteLine("Tree is empty");
+                return;
+            }
+            if (ptr != null)
+            {
+                inorder(ptr.leftchild);
+                Console.Write(ptr.info + "");
+                inorder(ptr.rightchild);
+            }
+        }
+        public void preorder (Node ptr)
+        {
+            if (ROOT == null)
+            {
+                Console.Write("Tree is Empty");
+                return;
+            }
+            if (ptr != null)
+            {
+                Console.WriteLine(ptr.info + "");
+                preorder(ptr.leftchild);
+                preorder(ptr);
+            }
+        }
+        public void postorder(Node ptr)
+        {
+            if (ROOT == null)
+            {
+                Console.WriteLine("Tree is empty");
+                return;
+            }
+            if (ptr != null)
+            {
+                postorder(ptr.leftchild);
+                postorder(ptr.rightchild);
+                Console.WriteLine(ptr.info + "");
+            }
+        }
+        static void Main(string[] args)
+        {
+            program x = new program();
+            while (true)
+            {
+                Console.WriteLine("\nMenu");
+                Console.WriteLine("1. Implementasi insert option");
+                Console.WriteLine("2. Perform inorder traversal");
+                Console.WriteLine("3. Perform preorder traversal");
+                Console.WriteLine("4. Perform postorder traversal");
+                Console.WriteLine("5. Exit");
+                Console.WriteLine("\nEnter your choice (1-5) :");
+                char ch = Convert.ToChar(Console.ReadLine());
+                Console.WriteLine();
+                switch(ch)
+                {
+                    case '1':
+                        {
+                            Console.Write("Enter a word ");
+                        }
+                        break;
+                    case '2':
+                        {
+                            x.inorder(x.ROOT);
+                        }
+                        break;
+                    case '3':
+                        {
+                            x.preorder(x.ROOT);
+                        }
+                        break;
+                    case '4':
+                        {
+                            x.postorder(x.ROOT);
+                        }
+                        break;
+                    case '5':
+                        return;
+                    default:
+                        {
+                            Console.WriteLine("Invalid option");
+                            break;
+                        }
+                }
+            }
+        }
     }
 }
 
