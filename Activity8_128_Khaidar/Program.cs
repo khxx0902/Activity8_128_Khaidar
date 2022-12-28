8076abc1004a8a59aafea8c709a7e1e77b22e49c
TA1128 <m.khaidar.ft21@mail.umy.ac.id>
2022-12-28 17:09:53 +0700

    add class node

--- Activity8_128_Khaidar/Program.cs
+++ Activity8_128_Khaidar/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree_128
+{
+    class Node
+    {
+        public string info;
+        public string leftchild;
+        public string rightchild;
+
+    }
+}
+
